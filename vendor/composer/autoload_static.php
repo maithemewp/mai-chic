efd5b46965b82809c065513dd77ce91d7485c5aa
Mike Hemberger <mike@thestizmedia.com>
2021-12-21 19:43:19 -0500

    make sure wpdi is loaded from engine

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,9 +12,6 @@ class ComposerStaticInit39b78a5d85819dbe2251967b341edc55
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'WP_Dependency_Installer' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer.php',
-        'WP_Dependency_Installer_Skin' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer-skin.php',
-        'WP_Dismiss_Notice' => __DIR__ . '/..' . '/afragen/wp-dismiss-notice/wp-dismiss-notice.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
